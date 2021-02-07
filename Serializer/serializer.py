5a7020d43a9b472235e567861077c6dafc1bb63b
31ayush05 <52788335+31ayush05@users.noreply.github.com>
2021-02-07 21:38:55 +0530

    Finalizing
    aldo made FUNCTION DESCRIPTION

--- Serializer/serializer.py
+++ Serializer/serializer.py
@@ -2,17 +2,61 @@ from os import path
 
 
 class dataBlock:
+    """
+    A python class capable of serializing data in the form of predefined data types that include -
+    INT, FLOAT, STR, BOOL, COMPLEX, LIST, TUPLE, DICT, SET
+    You can add infinitely many LISTS, TUPLES, DICTS or SETS inside other LISTS, TUPLES, DICTS or SETS and also
+    successfully retriving them by using the specific variable names.
 
-    def __init__(self, filePath, autoUpdate=False):
+    Author : Ayush Yadav
+    GitHub : https://github.com/31ayush05
+    Link to this repo : https://github.com/31ayush05/wSerializer
+    """
+
+    def __init__(self, filePath, autoSync=False):
+        """
+        dataBlock is the block of data in which you are going to store numerous values linked to their specific
+        names(variable names).
+        Then in future you can access the values of variables using the variable names
+
+        #RESTRICTIONS
+        don't use keywords reserved by this library as names or values
+        variable names cannot be TUPLES, DICT, LIST or SET
+
+        #RESERVED KEYWORDS
+        |↑|int|↑| |↓|int|↓| |↑int↑|
+        |↑|str|↑| |↓|str|↓| |↑str↑|
+        |↑|float|↑|  |↓|float|↓| |↑float↑|
+        |↑|bool|↑| |↓|bool|↓| |↑bool↑|
+        |↑|complex|↑| |↓|complex|↓| |↑complex↑|
+        |↑|dict|↑| |↓|dict|↓| |↑|dictInList|↑| |↓|dictInList|↓|
+        |↑|list|↑| |↓|list|↓| |↑l↑| |↓l↓|
+        |↑|tuple|↑| |↓|tuple|↓| |↑t↑| |↓t↓|
+        |↑|set|↑| |↓|set|↓| |↑s↑| |↓s↓|
+        :param filePath: Path of the file to which data is to be written
+        :param autoSync: if enabled it will update the dataFile as soon as new variables are added
+        """
         self.storeUpdate = None
         self.tempList = None
-        self.update = autoUpdate
+        self.update = autoSync
+        self.reservedKeywords = (
+            '|↑|int|↑|', '|↓|int|↓|', '|↑int↑|',
+            '|↑|str|↑|', '|↓|str|↓|', '|↑str↑|',
+            '|↑|float|↑|', '|↓|float|↓|', '|↑float↑|',
+            '|↑|bool|↑|', '|↓|bool|↓|', '|↑bool↑|',
+            '|↑|complex|↑|', '|↓|complex|↓|', '|↑complex↑|',
+            '|↑|dict|↑|', '|↓|dict|↓|',
+            '|↑|dictInList|↑|', '|↓|dictInList|↓|',
+            '|↑|list|↑|', '|↓|list|↓|', '|↑l↑|', '|↓l↓|',
+            '|↑|tuple|↑|', '|↓|tuple|↓|', '|↑t↑|', '|↓t↓|',
+            '|↑|set|↑|', '|↓|set|↓|', '|↑s↑|', '|↓s↓|'
+        )
         self.data = {}
         if not path.exists(filePath):
-            print('file does not exist')
-            self.dataFilePath = None
-        else:
-            self.dataFilePath = filePath
+            file = open(filePath, 'x')
+            file.close()
+        self.dataFilePath = filePath
+        self.Deserializer()
 
     def __str__(self):
         out = '\n' * 2 + '-- DATABASE --' + '\n' * 2
@@ -29,6 +73,45 @@ class dataBlock:
 
     def __getitem__(self, item):
         return self.data[str(item)]
+
+    def reset(self):
+        """
+        resets the dataBlock that is the data resets back to {}.
+
+        Moreover the data file is emptied.
+        
+        If the specified file does not exist a new blank file is created at the defined file path
+        :returns: nothing
+        """
+        if path.exists(self.dataFilePath):
+            file = open(self.dataFilePath, 'w')
+            file.truncate(0)
+            file.close()
+        else:
+            file = open(self.dataFilePath, 'x')
+            file.close()
+        self.__init__(self.dataFilePath, self.update)
+
+    def Add(self, name, value):
+        """
+        Function responsible for adding new variables with their values to the dataBlock.
+        Using this "name" the specified value can be accessed in future.
+        :param name: name of the variable
+        :param value: value to be stored in the variable
+        :return: nothing
+        """
+        if not (name in self.data):
+            if not (name in self.reservedKeywords):
+                if not (value in self.reservedKeywords):
+                    self.AddValue(name, value, None, 0, True)
+                    if self.update:
+                        self.Serialize()
+                else:
+                    print('variable value : ' + str(value) + ' is a reserved keyword')
+            else:
+                print('variable name : ' + str(name) + ' is a reserved keyword')
+        else:
+            print('Similar name exists cannot create duplicate names')
 
     def AddValue(self, name, value, dataSet, d, called=True):
         if called:
@@ -127,13 +210,28 @@ class dataBlock:
             if firstTime:
                 return lis
 
-    def Deserialize(self, usersCall=True, data=None):  # ↑ ↓
+    def Deserialize(self, printOutPut):
+        """
+        This function is responsible for Deserializing the data previously stored in the specified file.
+        Once the data is Deserialized it can be accessed anytime while the code is still running.
+        The code tries to Deserialize a file if exists once the dataBlock is created thus you don't need to call it
+        again and again.
+        :param printOutPut: if set True prints the dataSet every time called
+        :return: nothing
+        """
+        self.Deserializer(printOutPut)
+
+    def Deserializer(self, printOutPut=False, usersCall=True, data=None):
         if usersCall:
             self.data = {}
             if self.update:
                 self.storeUpdate = True
                 self.update = False
-            file = open(self.dataFilePath, 'r', encoding='UTF-8')
+            if path.exists(self.dataFilePath):
+                file = open(self.dataFilePath, 'r', encoding='UTF-8')
+            else:
+                print('Defined file path does not exist')
+                return None
             b = len(file.readlines())
             file.close()
         else:
@@ -162,7 +260,11 @@ class dataBlock:
         readDictInList = 0
         dictDepth = 0
         if usersCall:
-            file = open(self.dataFilePath, 'r', encoding='UTF-8')
+            if path.exists(self.dataFilePath):
+                file = open(self.dataFilePath, 'r', encoding='UTF-8')
+            else:
+                print('Defined file path does not exist')
+                return None
         for x in range(b):
             if usersCall:
                 tString = file.readline()
@@ -461,7 +563,7 @@ class dataBlock:
                                         readDictInList -= 1
                                         if readDictInList == 0:
                                             listStore = self.addToList(listDepth - 1, listStore,
-                                                                       self.Deserialize(False, dictToRead), True)
+                                                                       self.Deserializer(False, False, dictToRead), True)
                                         else:
                                             dictToRead.append(tString)
                                     else:
@@ -476,6 +578,9 @@ class dataBlock:
             self.update = True
         if not usersCall:
             return tempData
+        else:
+            if printOutPut:
+                print(self.__str__())
 
     def basicVarSerializer(self, key, value, outOfList=True):
         tpe = str(type(value))[8:-2]
@@ -554,6 +659,11 @@ class dataBlock:
         return out
 
     def Serialize(self):
+        """
+        Serializes the data stored by you in the dataSet using the Add() function that is it writes the data in the
+        specified text file
+        :return: nothing
+        """
         out = ''
         for x in self.data:
             tpe = str(type(self.data[x]))[8:-2]
@@ -565,18 +675,22 @@ class dataBlock:
                 out += self.serializeDICT(x, self.data[x], 0)
             else:
                 print('ERROR')
-        file = open(self.dataFilePath, 'w', encoding='UTF-8')
-        file.truncate(0)
-        file.write(out)
-        file.close()
+        if path.exists(self.dataFilePath):
+            f = open(self.dataFilePath, 'r', encoding='UTF-8')
+            f.truncate(0)
+            f.write(out)
+            f.close()
+        else:
+            f = open(self.dataFilePath, 'x', encoding='UTF-8')
+            f.truncate(0)
+            f.write(out)
+            f.close()
 
 
-a = dataBlock('E:\\testing.txt', True)
-a.data = {'a': 5, 'b': 6, 'c': [1, 2, 3, (4, 5, {1, 2, 3}), {'a': 1, 'b': 2, 'c': [4, 8, 8.0, complex(1, 2)]}]}
-a.Serialize()
-a.Deserialize()
-print(a.data)
+a = dataBlock('E://testing.txt', True)
 '''
-a.Deserialize()
-print(a)
+a.Add('name', 'JOHN')
+a.Add('subjects', ['maths', 'chemistry', 'physics', 'physical education', 'english', 'computer science'])
+a.Add('marks', [99.85, 91.1, 85, 100, 80, 100])
 '''
+print(a['subjects'])
